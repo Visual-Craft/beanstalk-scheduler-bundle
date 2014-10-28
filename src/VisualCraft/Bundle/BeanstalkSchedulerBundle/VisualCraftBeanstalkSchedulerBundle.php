5fc150f5c47650f68e4f90cc3faa9649fb186089
Anton Bakai <inso@i.ua>
2014-10-28 12:54:07 +0200

    minor

--- src/VisualCraft/Bundle/BeanstalkSchedulerBundle/VisualCraftBeanstalkSchedulerBundle.php
+++ src/VisualCraft/Bundle/BeanstalkSchedulerBundle/VisualCraftBeanstalkSchedulerBundle.php
@@ -13,6 +13,8 @@ class VisualCraftBeanstalkSchedulerBundle extends Bundle
      */
     public function build(ContainerBuilder $container)
     {
+        parent::build($container);
+
         $container->addCompilerPass(new RegisterWorkersCompilerPass());
     }
 }
